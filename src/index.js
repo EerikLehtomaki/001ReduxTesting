f9251a3abd77edea8d7c0853d39abcba7d7a5576
Eerik Lehtomäki <eejujole@student.jyu.fi>
2018-10-15 01:46:32 +0300

    almost done with first part

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyB-QwnKfx3TulwyV9aQxI4TXw8weXiHxFU';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+class App extends Component {
 
-// Create a new component. This component should produce
-// some html
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+            // this.setState({ videos: videos }); works when names are the same.
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <SearchBar />
+                <VideoDetail video={ this.state.selectedVideo } />
+                <VideoList
+                    onVideoSelect = { selectedVideo => this.setState( {selectedVideo} ) }
+                    videos = { this.state.videos } />
+            </div>
+        );
+    }
 }
 
-// Take these components generated HTML and put
-// it on the page
 ReactDOM.render(<App />, document.querySelector('.container'));
 
